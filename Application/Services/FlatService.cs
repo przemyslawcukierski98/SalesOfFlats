b7d919264a54b4c9eb7f46243bdcfdc0726ba395
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2022-07-17 18:21:02 +0200

    A DTO class for the flat was added along with a repository class

---
+++ Application/Services/FlatService.cs
@@ -0,0 +1,43 @@
+﻿using Application.Dto;
+using Application.Interfaces;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class FlatService : IFlatService
+    {
+        private readonly IFlatRepository _flatRepository;
+
+        public FlatService(IFlatRepository flatRepository)
+        {
+            _flatRepository = flatRepository;
+        }
+
+        public IEnumerable<FlatDto> GetAllFlats()
+        {
+            var flats = _flatRepository.GetAllFlats();
+            return flats.Select(flat => new FlatDto 
+            { 
+                Id = flat.Id,
+                Title = flat.Title,
+                Content = flat.Description
+            });
+        }
+
+        public FlatDto GetFlatById(int id)
+        {
+            var flat = _flatRepository.GetFlatById(id);
+            return new FlatDto()
+            {
+                Id = flat.Id,
+                Title = flat.Title,
+                Content = flat.Description
+            };
+        }
+    }
+}
