368611c6ff5c48af86692ae6fbc9e7f676188ccd
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2022-07-17 16:51:39 +0200

    Added Flat entity, delete unused files

---
+++ Domain/Entities/Flat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Flat
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Area { get; set; }
+        public int NumberOfRooms { get; set; }
+        public int Price { get; set; }
+        public string FormOfOwnership { get; set; }
+        public int Floor { get; set; }
+        public string StateOfCompletion { get; set; }
+        public string KindOfBalcony { get; set; }
+        public string ParkingSpace { get; set; }
+        public string Heating { get; set; }
+
+        public Flat()
+        {
+
+        }
+
+        public Flat(int id, string title, string description, double area,
+            int numberOfRooms, int price, string formOfOwnership,
+            int floor, string stateOfCompletion, string kindOfBalcony,
+            string parkingSpace, string heating)
+        {
+            (Id, Title, Description, Area, NumberOfRooms, Price, FormOfOwnership,
+                Floor, StateOfCompletion, KindOfBalcony, ParkingSpace, Heating) =
+            (id, title, description, area, numberOfRooms, price, formOfOwnership,
+            floor, stateOfCompletion, kindOfBalcony, parkingSpace, heating);
+        }
+    }
+}
