56c65b7e51f216f173af6d43d03523ee63ddd56d
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2022-07-18 21:41:03 +0200

    Added controller with methods to retrieve data (GET)

---
+++ WebApp/Controllers/FlatsController.cs
@@ -0,0 +1,42 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlatsController : ControllerBase
+    {
+        private readonly IFlatService _flatService;
+
+        public FlatsController(IFlatService flatService)
+        {
+            _flatService = flatService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var flats = _flatService.GetAllFlats();
+            return Ok(flats);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var flat = _flatService.GetFlatById(id);
+            if(flat == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(flat);
+        }
+
+
+    }
+}
