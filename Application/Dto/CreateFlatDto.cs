76bc76094e4b8ee719224212465d6a886dbd867d
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2022-07-21 14:03:26 +0200

    Mapping has been simplified

--- Application/Dto/CreateFlatDto.cs
+++ Application/Dto/CreateFlatDto.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Application.Mappings;
+using AutoMapper;
+using Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +9,14 @@ using System.Threading.Tasks;
 
 namespace Application.Dto
 {
-    public class CreateFlatDto
+    public class CreateFlatDto : IMap
     {
         public string Title { get; set; }
         public string Description { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<CreateFlatDto, Flat>();
+        }
     }
 }
