f7f30ca29a18f0b0451533d46903958bcd8fe29f
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2022-07-23 09:12:28 +0200

    Database reference implemented in repository class

--- Infrastructure/Repositories/FlatRepository.cs
+++ Infrastructure/Repositories/FlatRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Domain.Entities;
 using Domain.Interfaces;
+using Infrastructure.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,40 +11,43 @@ namespace Infrastructure.Repositories
 {
     public class FlatRepository : IFlatRepository
     {
-        private static readonly ISet<Flat> _flats = new HashSet<Flat>()
+        private readonly SalesContext _context;
+
+        public FlatRepository(SalesContext context)
         {
-            new Flat(1, "Title 1", "Description 1", 22.5, 1, 1600, "Pełna własność", 2,
-                "Wykończone", "brak", "brak", "miejskie"),
-        new Flat(2, "Title 2", "Description 2", 51.0, 3, 2800, "Pełna własność", 5,
-                "Wykończone", "balkon", "parking podziemny", "miejskie")
-        };
+            _context = context;
+        }
 
         public Flat Add(Flat flat)
         {
-            flat.Id = _flats.Count() + 1;
+            flat.Id = _context.Flats.Count() + 1;
             flat.Created = DateTime.UtcNow;
-            _flats.Add(flat);
+            _context.Flats.Add(flat);
+            _context.SaveChanges();
             return flat;
         }
 
         public void Delete(Flat flat)
         {
-            _flats.Remove(flat);
+            _context.Flats.Remove(flat);
+            _context.SaveChanges();
         }
 
         public IEnumerable<Flat> GetAllFlats()
         {
-            return _flats;
+            return _context.Flats;
         }
 
         public Flat GetFlatById(int id)
         {
-            return _flats.SingleOrDefault(x => x.Id == id);
+            return _context.Flats.SingleOrDefault(x => x.Id == id);
         }
 
         public void Update(Flat flat)
         {
             flat.LastModified = DateTime.UtcNow;
+            _context.Flats.Update(flat);
+            _context.SaveChanges();
         }
     }
 }
