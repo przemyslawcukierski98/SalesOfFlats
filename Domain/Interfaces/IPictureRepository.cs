c52f36881a92c33261c31efc81adff09e0b49e2e
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2022-08-04 20:50:37 +0200

    Implemented PictureRepository with methods for get picture, flat with picture, delete picture

--- Domain/Interfaces/IPictureRepository.cs
+++ Domain/Interfaces/IPictureRepository.cs
@@ -10,5 +10,8 @@ namespace Domain.Interfaces
     public interface IPictureRepository
     {
         Task<Picture> AddAsync(Picture picture);
+        Task<IEnumerable<Picture>> GetByFlatIdAsync(int flatId);
+        Task<Picture> GetPictureByIdAsync(int pictureId);
+        Task DeleteAsync(Picture picture);
     }
 }
--- Infrastructure/Repositories/PictureRepository.cs
+++ Infrastructure/Repositories/PictureRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Domain.Entities;
 using Domain.Interfaces;
 using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +24,24 @@ namespace Infrastructure.Repositories
             var createdPicture = await _context.AddAsync(picture);
             await _context.SaveChangesAsync();
             return createdPicture.Entity;
+        }
+
+        public async Task DeleteAsync(Picture picture)
+        {
+            _context.Pictures.Remove(picture);
+            await _context.SaveChangesAsync();
+            await Task.CompletedTask;
+        }
+
+        public async Task<IEnumerable<Picture>> GetByFlatIdAsync(int flatId)
+        {
+            return await _context.Pictures.Include(x => x.Flats)
+                .Where(x => x.Flats.Select(x => x.Id).Contains(flatId)).ToListAsync();
+        }
+
+        public async Task<Picture> GetPictureByIdAsync(int pictureId)
+        {
+            return await _context.Pictures.SingleOrDefaultAsync(x => x.Id == pictureId);
         }
     }
 }
