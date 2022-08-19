491487e87e1aff1063d820c7cbf9bd22ae8b570d
Przemyslaw Cukierski <przemyslawcukierski98@gmail.com>
2022-08-19 14:15:59 +0200

    A unit test has been added to check the functionality of retrieving information about an announcement with a given ID.

--- UnitTests/Services/FlatServiceTests.cs
+++ UnitTests/Services/FlatServiceTests.cs
@@ -3,6 +3,7 @@ using Application.Services;
 using AutoMapper;
 using Domain.Entities;
 using Domain.Interfaces;
+using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Moq;
 using System;
@@ -44,10 +45,45 @@ namespace UnitTests.Services
             await flatService.AddNewFlatAsync(flatDto);
 
             // Assert
-            flatRepositoryMock.Verify(x => x.AddAsync(It.IsAny<Flat>()), Times.Once); ;
+            flatRepositoryMock.Verify(x => x.AddAsync(It.IsAny<Flat>()), Times.Once); 
         }
 
-        
+        [Fact]
+        public async Task when_invoking_get_flat_async_if_should_invoke_get_async_on_flat_repository()
+        {
+            // Arrange
+            var flatRepositoryMock = new Mock<IFlatRepository>();
+            var mapperMock = new Mock<IMapper>();
+            var loggerMock = new Mock<ILogger<FlatService>>();
+
+            var flatService = new FlatService(flatRepositoryMock.Object,
+                mapperMock.Object, loggerMock.Object);
+
+            var flat = new Flat(1, "Title 1", "Description 1", 30, 1, 100000, "Własne", 1, "Stan surowy", "brak", "brak", "miejskie");
+            var flatDto = new FlatDto()
+            {
+                Id = flat.Id,
+                Title = flat.Title,
+                Description = flat.Description,
+                Area = flat.Area,
+                NumberOfRooms = flat.NumberOfRooms,
+                Price = flat.Price,
+                Floor = flat.Floor
+            };
+
+            flatRepositoryMock.Setup(x => x.GetFlatByIdAsync(flat.Id)).ReturnsAsync(flat);
+
+            // Act
+            var existingFlatDto = await flatService.GetFlatByIdAsync(flat.Id);
+
+            // Assert
+            flatRepositoryMock.Verify(x => x.GetFlatByIdAsync(flat.Id), Times.Once);
+            flatDto.Should().NotBeNull();
+            flatDto.Title.Should().NotBeNull();
+            flatDto.Title.Should().BeEquivalentTo(flat.Title);
+            flatDto.Description.Should().NotBeNull();
+            flatDto.Description.Should().BeEquivalentTo(flat.Description);
+        }
     }
 }
 
